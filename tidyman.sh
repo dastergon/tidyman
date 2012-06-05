d239197d211780f2dfc46c38c81eb4672b5ebd9c
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-06-05 03:08:52 +0300

    correctly handle folder argument and process arguments

--- tidyman.sh
+++ tidyman.sh
@@ -41,6 +41,48 @@ while true; do
             printf "error: unknown argument: %s\nTry %s -h for help.\n" "$1" "$name"
 			exit 1
 			;;
-		esac
-	done
-unset IFS
+        *)
+            break
+            ;;
+    esac
+    shift
+done
+
+folder="${1%/}"
+[ -n "$folder" ] || {
+    printf "error: %s: no directory given\n" "$name"
+    exit 1
+}
+[ -d "$folder" ] || {
+    printf "error: %s: can't find directory: %s\n" "$name" "$folder"
+    exit 1
+}
+
+if [ "$audio" -eq 1 ]; then
+    type="audio"
+    printf "Tidying %s files ...\n" "$type"
+    [ -d "$folder/tidyman/$type" ] || mkdir -p "$folder/tidyman/$type"
+    mv "$folder"/*.{mp3,wma,wav,acc,flac} "$folder/tidyman/$type" 2>/dev/null
+fi
+
+if [ "$docum" -eq 1 ]; then
+    type="document"
+    printf "Tidying %s files ...\n" "$type"
+    [ -d "$folder/tidyman/$type" ] || mkdir -p "$folder/tidyman/$type"
+    mv "$folder"/*.{doc,pdf,txt,odt,docx,xls,ppt} "$folder/tidyman/$type" 2>/dev/null
+fi
+
+if [ "$image" -eq 1 ]; then
+    type="image"
+    printf "Tidying %s files ...\n" "$type"
+    [ -d "$folder/tidyman/$type" ] || mkdir -p "$folder/tidyman/$type"
+    mv "$folder"/*.{png,gif,jpg,tif,jpeg,tiff,JPEG,TIFF} "$folder/tidyman/$type" 2>/dev/null
+fi
+
+if [ "$video" -eq 1 ]; then
+    type="video"
+    printf "Tidying %s files ...\n" "$type"
+    [ -d "$folder/tidyman/$type" ] || mkdir -p "$folder/tidyman/$type"
+    mv "$folder"/*.{3gp,mov,avi,wmv,webm,mpeg,ogg,mpg,mp4,flv,swf} "$folder/tidyman/$type" 2>/dev/null
+fi
+
